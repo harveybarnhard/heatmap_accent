3b13316a9879dca203944ec48ba16147087540a2
Harvey <harveybarnhard@uchicago.edu>
2019-03-23 17:40:32 -0500

    Update interpretor

---
+++ fit_converter.py
@@ -0,0 +1,9 @@
+"""
+    File name: fit_converter.py
+    Author: Harvey Barnhard
+    Date created: 3/23/2019
+    Date last modified: 3/23/2019
+    Python Version: 2.7
+"""
+
+import fitparse
--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,7 @@
 """
     File name: heatmap.py
     Author: Harvey Barnhard
-    Date created: 3/24/2019
+    Date created: 3/23/2019
     Date last modified: 3/23/2019
     Python Version: 3.7
 """
