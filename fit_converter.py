150ee5fad1c460a1711db4e96bc6d7cca713e154
Harvey <harveybarnhard@uchicago.edu>
2019-03-24 17:46:50 -0500

    Update fit_converter

--- fit_converter.py
+++ fit_converter.py
@@ -7,15 +7,15 @@
 """
 
 from fitparse import FitFile
+import os
+import pandas as pd
 
 # Read in .fit files
+fit_file = FitFile("C:\\Users\\Harvey\\Dropbox\\Projects\\heatmap\\data\\activities\\1850568882.fit")
 
-fitfile = FitFile("C:\\Users\\Harvey\\Dropbox\\Projects\\heatmap\\data\\activities\\1850568882.fit")
-
-# For each .fit file, obtain latitude and longitude positions
-
-for record in fitfile.get_messages():
+# For each .fit file, obtain latitude and longitude positions, and mean
+for record in fit_file.get_messages():
     if record.type == 'data':
         for field_data in record:
-            if field_data.name in ["position_lat", "position_long"]:
+            if field_data.name in ["position_lat", "position_long", "heart_rate"]:
                 print ' * {}: {}'.format(field_data.name, field_data.value)
