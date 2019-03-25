772cf2c48cf547fe7010a22cb325da8494e3bafd
Harvey <harveybarnhard@uchicago.edu>
2019-03-25 11:22:01 -0500

    Update fit_converter.py
    Corrected mistake where all heart and distance values were treated as missing

--- fit_converter.py
+++ fit_converter.py
@@ -24,9 +24,9 @@ tim = list()
 ind = list()
 
 # For each .fit file, obtain latitude and longitude positions, and mean
-i=1
+i=0
 for filename in os.listdir(path):
-    ind.append(i)
+    i += 1
     if filename.endswith(".fit"):
         fit_file = FitFile(filename)
         for record in fit_file.get_messages():
@@ -43,27 +43,33 @@ for filename in os.listdir(path):
                     if field_data.name == "position_long":
                         lon.append(field_data.value)
                         lon_flag = 1
-                    if field_data.name == "heart_rate" and lat_flag == 1 and lon_flag == 1:
-                        hrt.append(field_data.value)
+                    if field_data.name == "heart_rate":
+                        hrt_value = field_data.value
                         hrt_flag = 1
-                    if field_data.name == "distance" and lat_flag == 1 and lon_flag == 1:
-                        dis.append(field_data.value)
+                    if field_data.name == "distance":
+                        dis_value = field_data.value
                         dis_flag = 1
-                    if field_data.name == "timestamp" and lat_flag == 1 and lon_flag == 1:
-                        tim.append(field_data.value)
+                    if field_data.name == "timestamp":
+                        tim_value = field_data.value
                         tim_flag = 1
-                if lat_flag == 1 and lon_flag == 1 and hrt_flag == 0:
+                if lat_flag == 1 and lon_flag == 1:
+                    ind.append(i)
                     if hrt_flag == 0:
-                        hrt.append([-1])
+                        hrt.append(-1)
+                    else:
+                        hrt.append(hrt_value)
                     if dis_flag == 0:
-                        dis.append([-1])
+                        dis.append(-1)
+                    else:
+                        dis.append(dis_value)
                     if tim_flag == 0:
-                        tim.append([-1])
+                        tim.append(-1)
+                    else:
+                        tim.append(tim_value)
                     #if field_data.name in ["position_lat", "position_long", "heart_rate"]:
                     #    print ' * {}: {}'.format(field_data.name, field_data.value)
-    i+=1
 
 # Create dataframe and save as csv to working directory
-fit_data = {"LON":lon, "LAT":lat, "HRT":hrt, "DIS":dis, "TIM":tim}
+fit_data = {"IND":ind, "LON":lon, "LAT":lat, "HRT":hrt, "DIS":dis, "TIM":tim}
 fit_df = pd.DataFrame(fit_data)
 fit_df.to_csv("fit_df.csv", sep=",", index=False)
--- heatmap.py
+++ heatmap.py
@@ -2,24 +2,12 @@
     File name: heatmap.py
     Author: Harvey Barnhard
     Date created: 3/23/2019
-    Date last modified: 3/23/2019
+    Date last modified: 3/25/2019
     Python Version: 3.7
 """
 
-import sys
 import os
-import csv
-import xml.etree.ElementTree as ET
 
-# Set Working Directory
-cd = os.path.dirname(os.path.abspath("C:\\Users\\Harvey\\Dropbox\\Projects\\heatmap\\data\\activities\\2132509368.tcx"))
-
-# Read TCX File
-tcx_file = ET.parse(open("C:\\Users\\Harvey\\Dropbox\\Projects\\heatmap\\data\\activities\\2132509368.tcx").readlines())
-
-# Set columns
-
-dom = lxml.etree.parse(os.path.join(cd, tcx_file))
 
 
 
